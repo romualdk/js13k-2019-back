f3fcf7e8413ff37e1c19065e2a94e0c9fab56e7c
romualdk <romuald.kowalczyk@gmail.com>
2019-08-21 08:35:42 +0200

    Minification

--- src/js/Tset.js
+++ src/js/Tset.js
@@ -1,10 +1,7 @@
 export const Tset = (img, tw, th, cols) => ({ img, tw, th, cols })
-export const tile = (tset, i) => {
-  const j = i - 1
-  return {
-    x: j % tset.cols * tset.tw,
-    y: Math.floor(j / tset.cols) * tset.th,
-    w: tset.tw,
-    h: tset.th
-  }
-}
+export const tile = (tset, i) => ({
+  x: (i - 1) % tset.cols * tset.tw,
+  y: Math.floor((i - 1) / tset.cols) * tset.th,
+  w: tset.tw,
+  h: tset.th
+})
