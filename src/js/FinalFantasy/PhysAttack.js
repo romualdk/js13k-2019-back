873263dbb486da8ff422c7c3793a7ff057d33981
romualdk <romuald.kowalczyk@gmail.com>
2019-08-27 19:50:54 +0200

    names to first letter lowercase

--- src/js/FinalFantasy/PhysAttack.js
+++ src/js/FinalFantasy/PhysAttack.js
@@ -1,15 +1,15 @@
 import { R } from './R.js'
 
-export const physDmg = (Attacker, Defender) => {
-  const Chance = 168 + Attacker.Hit - Defender.Eva
+export const physDmg = (attacker, defender) => {
+  const chance = 168 + attacker.hit - defender.eva
   const n = R(0, 200)
-  const hit = (n === 0 || n <= Chance) && n !== 200
+  const hit = (n === 0 || n <= chance) && n !== 200
 
   if (!hit) {
     return 0 // miss
   }
 
-  const Critical = n === 0 || n <= Attacker.Crt
+  const critical = n === 0 || n <= attacker.crt
 
-  return (Critical + 1) * R(Attacker.Att, 2 * Attacker.Att) - Defender.Def
+  return (critical + 1) * R(attacker.att, 2 * attacker.att) - defender.def
 }
